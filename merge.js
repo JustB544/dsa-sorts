d4f77a22e824c54ea06bfa5de02228c40feb09d9
Breyton Pabst <breytonpabst@gmail.com>
2023-11-19 13:31:35 -0700

    merge sort

---
+++ merge.js
@@ -0,0 +1,34 @@
+function merge(a, b) {
+    const arr = [];
+    let i = 0;
+    let j = 0;
+    while (j !== b.length && i !== a.length){
+        if (a[i] <= b[j]){
+            arr.push(a[i]);
+            i++;
+        }
+        else {
+            arr.push(b[j]);
+            j++;
+        }
+    }
+    while (i < a.length){
+        arr.push(a[i]);
+        i++;
+    }
+    while (j < b.length){
+        arr.push(b[j]);
+        j++;
+    }
+    return arr;
+}
+
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+    const mid = Math.floor(arr.length/2);
+    const left = mergeSort(arr.slice(0, mid));
+    const right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+}
+
+module.exports = { merge, mergeSort};
